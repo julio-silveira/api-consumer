e2834c0f1d6ee203045286f583fb05306e5b38ed
julio-silveira <julioqueiroz15@gmail.com>
2023-01-06 10:42:44 -0300

    feat: userservice created

--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { UserDto } from './dto/user.dto';
+import { User, UserDocument } from './schemas/user.schema';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
+  ) {}
+
+  async findOne(username: string): Promise<User | null> {
+    return await this.userModel.findOne({ username });
+  }
+
+  private async checkUsernameAvailability(username: string): Promise<boolean> {
+    const user = await this.findOne(username);
+    return user !== null;
+  }
+
+  async hashPassword(password: string): Promise<string> {
+    return password;
+  }
+
+  async create({ username, password }: UserDto) {
+    const isUsernameAvaliable = await this.checkUsernameAvailability(username);
+    if (!isUsernameAvaliable)
+      throw new BadRequestException('Username aleready taken');
+
+    const hashedPassword = await this.hashPassword(password);
+
+    const userDataToSave: User = { username, password: hashedPassword };
+
+    const newUser = await this.userModel.create(userDataToSave);
+
+    return { message: `${newUser.username} was successfully registered!` };
+  }
+}
