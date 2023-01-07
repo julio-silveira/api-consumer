fb33ffa5e0ec6b7291e4335a2c5a3e9e65362027
julio-silveira <julioqueiroz15@gmail.com>
2023-01-07 10:40:11 -0300

    inputs on login page

--- web/src/pages/Login/Login.tsx
+++ web/src/pages/Login/Login.tsx
@@ -1,10 +1,20 @@
-import { Box } from '@mui/material'
+import {
+  Box,
+  Checkbox,
+  FormControlLabel,
+  Paper,
+  Stack,
+  TextField
+} from '@mui/material'
 import React from 'react'
+import { primaryColor } from '../../Theme/colorPalette'
 
 const Login: React.FC = () => {
   return (
     <Box
       sx={{
+        backgroundImage:
+          'url("https://finestwp.co/demos/wp/shadepro/wp-content/uploads/2020/10/pattern-2.png")',
         width: '100%',
         height: '100vh',
         display: 'flex',
@@ -12,7 +22,31 @@ const Login: React.FC = () => {
         justifyContent: 'center'
       }}
     >
-      Login
+      <Paper
+        component={Stack}
+        spacing={{ xs: 0, sm: 1 }}
+        width={{ xs: '90%', sm: '30%' }}
+      >
+        <Stack component="form" spacing={1}>
+          <Box pb={1}>
+            <img
+              width={250}
+              src="https://www.sharenergy.com.br/wp-content/uploads/2022/12/logo_color.png"
+              alt="ShareEnergyLogo"
+            />
+          </Box>
+          <TextField
+            sx={{ color: primaryColor }}
+            type="text"
+            label="Nome de Usuário"
+          />
+          <TextField type="password" label="Senha" />
+          <FormControlLabel
+            control={<Checkbox size="small" />}
+            label="Lembrar de mim?"
+          />
+        </Stack>
+      </Paper>
     </Box>
   )
 }
