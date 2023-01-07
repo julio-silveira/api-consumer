43e0d4662ad8e0446b74861448d15772b86ea18a
julio-silveira <julioqueiroz15@gmail.com>
2023-01-06 22:58:04 -0300

    chore: frontend started with vite

---
+++ app/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+
+
+export default ({mode}) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+return defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: Number(process.env.APP_PORT || 5173)
+  }
+})
+}
